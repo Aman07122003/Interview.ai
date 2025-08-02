c60bf8ece8e6352b85d4b1997072822d4da793ce
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-08-02 18:57:36 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    outDir: 'build', // This tells Vite to output to 'build' instead of 'dist'
+    emptyOutDir: true // This will clear the directory before each build
+  }
 })
