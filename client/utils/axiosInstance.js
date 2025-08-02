a93d86b634f7d1ba3c73ac6f2c0c1bcab46e801c
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-08-02 18:43:40 +0530

    Update axiosInstance.js

--- client/utils/axiosInstance.js
+++ client/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-ai-1nul.onrender.com",
     credentials: true,
 });
 
